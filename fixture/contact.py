4a8b3a8458bfe90f3fad73c2889e9c1ee34713ce
Łukasz Niewęgłowski <lukasz.nieweglowski86@gmail.com>
2022-03-01 22:35:03 +0100

    code decomposition

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,53 +1,13 @@
-from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
 
-class Application:
+class ContactHelper:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+    def __init__(self, app):
+        self.app = app
 
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def add_new_contact(self, contact):
-        wd = self.wd
+    def add(self, contact):
+        wd = self.app.wd
         # initiate adding new contact
         wd.find_element_by_link_text("add new").click()
         # new contact form - name and first address
@@ -129,16 +89,5 @@ class Application:
         self.back_to_home_page()
 
     def back_to_home_page(self):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def destroy(self):
-        self.wd.quit()
---
+++ test/test_add_contact.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import pytest
+from model.contact import Contact
+from fixture.application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.add(Contact(firstname="Qwerty", middlename="Qwe", lastname="Asda",
+                            nickname="Lotr", title="Agent", company="Deep Space Core Mining",
+                            address="Elm Street", homenumber="123123123", mobilenumber="234234234",
+                            worknumber="345345345", fax="456456456", email="qwerty.asda@gmail.com",
+                            email2="qwerty.asda+2@gmail.com", email3="qwerty.asda+3@gmail.com",
+                            homepage="google.com", bday="19", bmonth="December", byear="1977",
+                            aday="24", amonth="November", ayear="2005", address2="Hamburger Hill",
+                            phone2="567567567", notes="Lorem ipsum"))
+    app.session.logout()
