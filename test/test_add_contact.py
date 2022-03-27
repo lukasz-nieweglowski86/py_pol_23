ed530f5fb955a015e1834b786a44a4bbdcb2f9f0
Łukasz Niewęgłowski <lukasz.nieweglowski86@gmail.com>
2022-03-27 21:57:43 +0200

    ex15

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,17 +1,54 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_phone(prefix, maxlen):
+    phone_numbers = string.digits
+    return prefix + "".join([random.choice(phone_numbers) for i in range(random.randrange(maxlen))])
+
+
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
+          "December"]
+
+
+testdata = [Contact(firstname=random_string("firstname: ", 8),
+                    middlename=random_string("middlename: ", 8),
+                    lastname=random_string("lastname: ", 12),
+                    nickname=random_string("nickname: ", 8),
+                    title=random_string("title: ", 6),
+                    company=random_string("company: ", 18),
+                    address=random_string("address: ", 10),
+                    homenumber=random_phone("homenumber: ", 12),
+                    mobilenumber=random_phone("mobilenumber: ", 12),
+                    worknumber=random_phone("worknumber: ", 12),
+                    phone2=random_phone("phone2: ", 12),
+                    fax=random_phone("fax: ", 12),
+                    email=random_string("email: ", 16),
+                    email2=random_string("email2: ", 16),
+                    email3=random_string("email3: ", 16),
+                    homepage=random_string("homepage: ", 20),
+                    bday=str(random.randint(1, 29)),
+                    bmonth=random.choice(months),
+                    byear=str(random.randint(1940, 1980)),
+                    aday=str(random.randint(1, 29)),
+                    amonth=random.choice(months),
+                    ayear=str(random.randint(1981, 2021)),
+                    address2=random_string("address2: ", 16),
+                    notes=random_string("notes: ", 50))
+            for i in range(5)]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contacts_list()
-    contact = Contact(firstname="Qwerty", middlename="Qwe", lastname="Asda",
-                      nickname="Lotr", title="Agent", company="Deep Space Core Mining",
-                      address="Elm Street", homenumber="123123123", mobilenumber="234234234",
-                      worknumber="345345345", fax="456456456", email="qwerty.asda@gmail.com",
-                      email2="qwerty.asda+2@gmail.com", email3="qwerty.asda+3@gmail.com",
-                      homepage="google.com", bday="19", bmonth="December", byear="1977",
-                      aday="24", amonth="November", ayear="2005", address2="Hamburger Hill",
-                      phone2="567567567", notes="Lorem ipsum")
     app.contact.add(contact)
     new_contacts = app.contact.get_contacts_list()
     assert len(old_contacts) + 1 == len(new_contacts)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
+import pytest
 from model.group import Group
+import random
+import string
 
 
-def test_add_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")] + \
+           [Group(name=random_string("name: ", 8), header=random_string("header: ", 12),
+                  footer=random_string("footer: ", 12))
+            for i in range(5)]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="test group", header="group header", footer="group footer")
     app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-# def test_add_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group = Group(name="", header="", footer="")
-#     app.group.create(group)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
