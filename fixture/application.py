1630a32eb92f3f1078233cd24018fb9fd79be6f4
Łukasz Niewęgłowski <lukasz.nieweglowski86@gmail.com>
2022-03-30 21:18:06 +0200

    ex16

--- fixture/application.py
+++ fixture/application.py
@@ -7,11 +7,19 @@ from selenium.webdriver.support.ui import Select
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -24,7 +32,7 @@ class Application:
         wd = self.wd
         if not (wd.current_url.endswith("localhost/addressbook/") and
                 len(wd.find_elements_by_name("searchstring")) > 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def set_value(self, field_name, value):
         wd = self.wd
